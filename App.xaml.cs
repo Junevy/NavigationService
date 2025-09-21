f8fe62c7a372f2be6d0c7ac2fbf9ce72781fcc1b
Junevy <junev.chung@gmail.com>
2025-09-22 01:05:02 +0800

    Navigation method of secend.

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace NavigationService
+{
+    public partial class App : Application
+    {
+        public IServiceProvider Services { get; }
+        public static new App Current = (App)Application.Current;
+        public App()
+        {
+            var container = new ServiceCollection();
+
+            container.AddSingleton<MainWindow>();
+            container.AddSingleton<MainWindowViewModel>();
+            container.AddTransient<LoginViewModel>();
+            container.AddTransient<HomeViewModel>();
+            container.AddSingleton<UserService>();
+            container.AddSingleton<INaviService, NaviService>();
+
+            Services = container.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = Services.GetService<MainWindow>();
+            MainWindow!.Show();
+        }
+    }
+
+}
